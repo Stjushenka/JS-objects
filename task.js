59c33ee1ffeb1db7c1746ce750c141b8a5304ea8
Stjushenka <stjushenka@gmail.com>
2022-07-31 12:11:52 +0300

    add

---
+++ task.js
@@ -0,0 +1,45 @@
+'use strict';
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+};
+
+
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+
+Student.prototype.addMarks = function (...rest) { 
+  if(this.marks === undefined){ 
+    this.marks = [];
+    this.marks.push(...rest);
+  } else {
+    this.marks.push(...rest);
+  }
+}
+
+Student.prototype.getAverage = function () { 
+  if (this.marks === undefined) {
+    return null}
+    else { 
+      return (this.marks.reduce ((previous, next) => previous + next))/this.marks.length
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject; 
+  delete this.marks; 
+  this.excluded = reason;
+}
